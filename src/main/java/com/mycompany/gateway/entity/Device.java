5728fde4ecfc2631e359306a086b3b01262b4c76
reiniersf <reytosan0088@gmail.com>
2020-07-15 00:38:24 +0300

    It works!!

--- src/main/java/com/mycompany/gateway/entity/Device.java
+++ src/main/java/com/mycompany/gateway/entity/Device.java
@@ -6,51 +6,42 @@
 
 package com.mycompany.gateway.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-import java.io.Serializable;
-
 /**
- *
  * @author lallave
  */
 @Entity
-@Table(name="device")
+@Table(name = "device")
 public class Device implements Serializable {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
-	
-    @Column(name="seller")
+
+    @Column(name = "seller")
     private String seller;
-	
-    @Column(name="creationdate")
+
+    @Column(name = "creationdate")
     @CreationTimestamp
     private Timestamp creationdate;
-    
+
     @ManyToOne
-    @JoinColumn(name="serialnumber")
+    @JoinColumn(name = "serialnumber")
     private Gateway gateway;
-    
+
     public Device() {
 
-	}
+    }
 
-	public Device(String seller, Timestamp creationdate) {
-		this.seller = seller;
-		this.creationdate = creationdate;
-	}
+    public Device(String seller, Timestamp creationdate) {
+        this.seller = seller;
+        this.creationdate = creationdate;
+    }
 
     public Device(long id, String seller, Timestamp creationdate, Gateway gateway) {
         this.id = id;
@@ -58,37 +49,37 @@ public class Device implements Serializable {
         this.creationdate = creationdate;
         this.gateway = gateway;
     }
-        
-	
-	public long getId(){
-		return id;
-	}
-    
-	public void setId(long id){
-		this.id = id;
-	}
-	
-	public String getSeller(){
-		return seller;
-	}
-	
-	public void setSeller(String seller){
-		this.seller = seller;
-	}
-	
-	public Timestamp getCreationdate(){
-		return creationdate;
-	}
-	
-	public void setCreationdate(Timestamp creationdate){
-		this.creationdate = creationdate;
-	}
-        
-	
-	@Override
-	public String toString(){
-		return "Device [id = "+ id +", seller = "+ seller +", creationdate = "+ creationdate +"]";
-	}
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public Timestamp getCreationdate() {
+        return creationdate;
+    }
+
+    public void setCreationdate(Timestamp creationdate) {
+        this.creationdate = creationdate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Device [id = " + id + ", seller = " + seller + ", creationdate = " + creationdate + "]";
+    }
 
     /**
      * @return the gateway
